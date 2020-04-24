c026b7615de1ad86f6ee49619470a19285ebff4a
Krutik <krutik@hooq.tv>
2020-04-24 08:19:53 +0800

    Validate user input data

--- app/src/main/java/com/example/calculator/data/Validator.kt
+++ app/src/main/java/com/example/calculator/data/Validator.kt
@@ -4,6 +4,52 @@ import com.example.calculator.domain.data.IValidator
 
 class Validator : IValidator {
     override fun validate(expression: String): Boolean {
+        if (invalidStart(expression)) return false
+        if (invalidEnd(expression)) return false
+
+        if (hasMultipleDecimals(expression)) return false
         return true
+    }
+
+    private fun invalidStart(expression: String): Boolean {
+        return when {
+            expression.startsWith("+") -> true
+            expression.startsWith("-") -> true
+            expression.startsWith("*") -> true
+            expression.startsWith("/") -> true
+            expression.startsWith(".") -> true
+            else -> false
+        }
+    }
+
+    private fun isConcurrentDecimal(current: Char, next: Char): Boolean {
+        if (current.toString() == "." && next.toString() == ".") {
+            return true
+        }
+        return false
+    }
+
+    private fun hasMultipleDecimals(expression: String): Boolean {
+        expression.indices
+            .forEach {
+                if (it < expression.lastIndex) {
+                    if (isConcurrentDecimal(expression[it], expression[it + 1])) {
+                        return true
+                    }
+                }
+            }
+
+        return false
+    }
+
+    private fun invalidEnd(expression: String): Boolean {
+        return when {
+            expression.endsWith("+") -> true
+            expression.endsWith("-") -> true
+            expression.endsWith("*") -> true
+            expression.endsWith("/") -> true
+            expression.endsWith(".") -> true
+            else -> false
+        }
     }
 }
---
+++ app/src/test/java/com/example/calculator/data/ValidatorTest.kt
@@ -0,0 +1,52 @@
+package com.example.calculator.data
+
+
+import junit.framework.Assert.assertFalse
+import junit.framework.TestCase.assertTrue
+import org.junit.Test
+
+class ValidatorTest {
+    //Test objet
+    private val validator = Validator()
+
+
+    @Test
+    fun validExpressionTestOne() {
+        assertTrue(validator.validate(ADD_OPERATOR_EXPRESSION))
+    }
+
+    @Test
+    fun validExpressionTestTwo() {
+        assertTrue(validator.validate(Companion.MULTIPLE_OPERATOR_EXPRESSION))
+    }
+
+    @Test
+    fun invalidExpressionTestOne() {
+        assertFalse(validator.validate(ADD_OPERATOR_END_EXPRESSION))
+    }
+
+    @Test
+    fun invalidExpressionTestTwo() {
+        assertFalse(validator.validate(ADD_OPERATOR_START_EXPRESSION))
+    }
+
+
+    @Test
+    fun invalidExpressionTestFour() {
+        assertFalse(validator.validate(DOT_EXPRESSION))
+    }
+
+    @Test
+    fun invalidExpressionTestFive() {
+        assertFalse(validator.validate(MULTIPLE_DOT_EXPRESSION))
+    }
+
+    companion object {
+        const val MULTIPLE_OPERATOR_EXPRESSION = "5+3-4*5/3"
+        const val ADD_OPERATOR_EXPRESSION = "3+4"
+        const val ADD_OPERATOR_END_EXPRESSION = "4+"
+        const val ADD_OPERATOR_START_EXPRESSION = "+5"
+        const val DOT_EXPRESSION = "."
+        const val MULTIPLE_DOT_EXPRESSION = "3..4+2"
+    }
+}
